ada703be3dbb18a927cc2352e57d725c8a5d95b9
AbdAlwahab <AbdAlwahabAbuKhalil>
2023-04-17 09:22:44 +0300

    Kotlin

---
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.homework1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.*
+import android.*
+import android.view.MenuItem
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.DialogFragment
+
+import android.widget.Toast
+import org.w3c.dom.Text
+
+var tester=1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        val calc=calculator()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.showFragment,calc)
+            commit()
+        }
+
+
+
+
+
+    }
+    override fun onCreateOptionsMenu(menu:Menu):Boolean{
+        val inflater=menuInflater
+        inflater.inflate(R.menu.star_menu,menu)
+        inflater.inflate(R.menu.resturent,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        var id: String = item.itemId.toString()
+        //Toast.makeText(this,id,Toast.LENGTH_SHORT).show()
+
+        if (id == "2131231137") {
+            var RateDialog = RateDialogClass()
+            RateDialog.show(supportFragmentManager, "Rate")
+        } else {
+            val calc = calculator()
+            val product = menue()
+            if (tester == 1) {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.showFragment, product)
+                    commit()
+                }
+                tester = 0
+
+            } else {
+
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.showFragment, calc)
+                    commit()
+                }
+                tester = 1
+            }
+
+        }
+        return true;
+    }
+}
